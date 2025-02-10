f45273005f7f8b0bf1b773c7943ee33d6e70d7ae
= <=>
2025-02-10 10:28:16 -0300

    Primer Commit

---
+++ utils.mjs
@@ -0,0 +1,29 @@
+import fs from "fs";
+class Superheroe {
+  constructor(
+    id,
+    nombreSuperheroe,
+    nombreReal,
+    nombreSociedad,
+    edad,
+    planetaOrigen,
+    debilidad,
+    poder,
+    habilidadEspecial,
+    aliado,
+    enemigo
+  ) {
+    this.id = id;
+    this.nombreSuperheroe = nombreSuperheroe;
+    this.nombreReal = nombreReal;
+    this.nombreSociedad = nombreSociedad;
+    this.edad = edad;
+    this.planetaOrigen = planetaOrigen;
+    this.debilidad = debilidad;
+    this.poder = poder;
+    this.habilidadEspecial = habilidadEspecial;
+    this.aliado = aliado;
+    this.enemigo = enemigo;
+  }
+}
+export function leerSuperheroes()
