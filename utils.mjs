3cb3e8b8c58b93d848e956c930f27def06a28205
= <=>
2025-02-11 10:21:23 -0300

    Segundo commit

--- utils.mjs
+++ utils.mjs
@@ -26,4 +26,13 @@ class Superheroe {
     this.enemigo = enemigo;
   }
 }
-export function leerSuperheroes()
+export function leerSuperheroes(rutaArchivo) {
+  const datos = fs.readFileSync(rutaArchivo,'utf-8')
+  const superheroeArray = JSON.parse(datos);
+
+  const superheroes = superheroeArray.map(hero => new Superheroe(hero.id, hero.nombreSuperheroe, hero.nombreReal, hero.nombreSociedad, hero.edad, hero.planetaOrigen, hero.debilidad, hero.poder, hero.habilidadEspecial, hero.aliado, hero.enemigo))
+    
+
+  superheroes.sort((a, b) => a.nombreSuperheroe.localeCompare(b.nombreSuperheroe))
+  return superheroes;
+}
