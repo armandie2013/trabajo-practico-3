ed28d213beb62258732297c7b5ad53950b3eb08e
= <=>
2025-02-11 12:43:02 -0300

    agregadoss

--- utils.mjs
+++ utils.mjs
@@ -1,4 +1,4 @@
-import fs from "fs";
+import fs, { fstatSync } from "fs";
 class Superheroe {
   constructor(
     id,
@@ -27,11 +27,25 @@ class Superheroe {
   }
 }
 export function leerSuperheroes(rutaArchivo) {
-  
-  const datos = fs.readFileSync(rutaArchivo,'utf-8')
+
+  const datos = fs.readFileSync(rutaArchivo, 'utf-8')
   const superheroeArray = JSON.parse(datos);
   const superheroes = superheroeArray.map(hero => new Superheroe(hero.id, hero.nombreSuperheroe, hero.nombreReal, hero.nombreSociedad, hero.edad, hero.planetaOrigen, hero.debilidad, hero.poder, hero.habilidadEspecial, hero.aliado, hero.enemigo))
   superheroes.sort((a, b) => a.nombreSuperheroe.localeCompare(b.nombreSuperheroe))
   return superheroes;
+
+}
+
+export function agregarSuperheroes(rutaOriginal, rutaNuevos) {
   
+  const datosOriginales = fs.readFileSync(rutaOriginal, 'utf-8');
+  const datosNuevos = fs.readFileSync(rutaNuevos, 'utf-8');
+  const superHeroesOrgiginales = JSON.parse(datosOriginales);
+  const nuevosSuperheroes = JSON.parse(datosNuevos);
+  const instanciasNuevos = nuevosSuperheroes.map(hero => new Superheroe(hero.id, hero.nombreSuperheroe, hero.nombreReal, hero.nombreSociedad, hero.edad, hero.planetaOrigen, hero.debilidad, hero.poder, hero.habilidadEspecial, hero.aliado, hero.enemigo));
+
+
+  const listaActualizada = [...superHeroesOrgiginales, ...instanciasNuevos];
+  fs.writeFileSync(rutaOriginal, JSON.stringify(listaActualizada, null, 2), 'utf8');
+  console.log('Lista de superheroes actualizada con exito.')
 }
